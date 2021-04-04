a0742be6fb685f91fcd28990d400532ff2db3692
Abe Kohandel <abe.kohandel@gmail.com>
2021-04-03 22:31:22 -0700

    Restructure messages
    Use chain of responsibility with registered generic template builders
    to build messages from incoming steam.

--- include/client.h
+++ include/client.h
@@ -9,6 +9,7 @@
 using std::deque;
 using std::string;
 using std::unique_ptr;
+using std::make_unique;
 using boost::asio::io_context;
 using boost::asio::ip::tcp;
 using std::thread;
@@ -25,15 +26,123 @@ class Client
         string real_name;
         deque<string> write_messages;
         boost::asio::streambuf read_buffer;
+        const char *message_delimiter = "\r\n";
+        builder *handler;
 
-        void start_runner();
-        void send_message(Message::Message message);
-        void do_write();
-        void do_read();
+        void start_runner() {
+            runner = std::make_unique<std::thread>(
+                [this]() {
+                    executer.run();
+                }
+            );
+        }
+
+        void send_message(unique_ptr<message> &msg) {
+            auto text = msg->serialize().append(message_delimiter);
+            auto initiate_write = write_messages.empty();
+            write_messages.push_back(text);
+            if (initiate_write)
+                do_write();
+        }
+
+        void do_read() {
+            boost::asio::async_read_until(*socket,
+                read_buffer, message_delimiter,
+                [this](boost::system::error_code ec, std::size_t size) {
+                    if (ec)
+                        socket->close();
+                    else {
+                        std::istream reader(&read_buffer);
+                        std::vector<char> text(size);
+                        reader.read(text.data(), size);
+
+                        do_read();
+
+                        try {
+                            unique_ptr<message> msg = handler->build(string{text.begin(), text.end() - strlen(message_delimiter)});
+
+                            // HACK: just respond to ping for now
+                            // need to restructure messages and allow for defining
+                            // handlers for message types
+                            ping *ping_msg = dynamic_cast<ping*>(msg.get());
+                            if (ping_msg) {
+                                unique_ptr<message> pong_msg = make_unique<pong>(ping_msg->servers);
+                                send_message(pong_msg);
+                            }
+                        } catch (std::invalid_argument e) {
+                            std::cerr << e.what() << std::endl;
+                        }
+                    }
+                });
+        }
+        void do_write() {
+            string msg = write_messages.front();
+
+            std::cout << "Sending: " << msg << std::endl;
+
+            boost::asio::async_write(*socket,
+                boost::asio::buffer(msg, msg.length()),
+                [this](boost::system::error_code ec, std::size_t)
+                {
+                    if (ec)
+                        socket->close();
+                    else {
+                        write_messages.pop_front();
+                        if (!write_messages.empty())
+                            do_write();
+                    }
+                }
+            );
+        }
 
     public:
-        Client(string host, string service);
-        ~Client();
-        void connect_as(string user_name, string real_name);
-        void disconnect();
+        Client(string host, string service) : host(host), service(service) {
+            (handler = new template_builder<pass>())
+                ->set_next(new template_builder<nick>())
+                ->set_next(new template_builder<user>())
+                ->set_next(new template_builder<ping>())
+                ->set_next(new template_builder<pong>())
+                ->set_next(new template_builder<notice>())
+            ;
+        }
+
+        ~Client() {
+            delete handler;
+            if (runner)
+                runner->join();
+        }
+
+        void connect_as(string user_name, string real_name) {
+            user_name = user_name;
+            real_name = real_name;
+
+            tcp::resolver resolver(executer);
+
+            auto endpoints = resolver.resolve(host, service);
+            socket = make_unique<tcp::socket>(executer);
+
+            boost::asio::async_connect(*socket, endpoints,
+                [this, user_name, real_name](boost::system::error_code ec, tcp::endpoint)
+                {
+                    if (ec)
+                        socket->close();
+                    else {
+                        do_read();
+                        unique_ptr<message> msg = make_unique<pass>("password");
+                        send_message(msg);
+                        msg = make_unique<nick>(user_name);
+                        send_message(msg);
+                        msg = make_unique<nick>(real_name);
+                        send_message(msg);
+                    }
+                }
+            );
+
+            start_runner();
+        }
+
+        void disconnect() {
+            executer.stop();
+            socket->close();
+        }
 };
--- include/message.h
+++ include/message.h
@@ -2,51 +2,240 @@
 
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 using std::vector;
 using std::string;
+using std::unique_ptr;
+using std::make_unique;
 
-namespace Message {
-    class Message {
-        private:
-            void extract_prefix(string &messsage);
-            void extract_command(string &messsage);
-            void extract_parameters(string &message);
+class build_digest {
+	public:
+		string prefix;
+		string command;
+		vector<string> params;
+		build_digest(string text) {
+			size_t position = 0;
 
-        public:
-            // HACK: need to remove these and restructure
-            // messages
-            string command;
-            string prefix;
-            vector<string> parameters;
+			if (text.size() > 0 && text.at(position) == ':') {
+				position = 1;
+				auto prefix_end = text.find_first_of(' ');
+				if (prefix_end == string::npos) {
+					prefix = text.substr(position);
+					position = string::npos;
+				} else {
+					prefix = text.substr(position, prefix_end - position);
+					position = prefix_end + 1;
+				}
+			}
 
-            Message() = default;
-            Message(string message);
-            void parse(string message);
-            string serialize() const;
-    };
+			if (position < text.size()) {
+				auto command_end = text.find_first_of(' ', position);
+				if (command_end == string::npos) {
+					command = text.substr(position);
+					position = string::npos;
+				} else {
+					command = text.substr(position, command_end - position);
+					position = command_end + 1;
+				}
+			}
+			if (command.size() < 1)
+				throw std::invalid_argument(string{"Malformed message: "} + text);
 
-    class pass: public Message
-    {
-        public:
-            pass();
-    };
+			while (position < text.size()) {
+				auto param_end = text.find_first_of(" :", position);
+				if (param_end == string::npos) {
+					params.push_back(text.substr(position, param_end - position));
+					return;
+				} else {
+					switch (text.at(param_end)) {
+						case ':':
+							params.push_back(text.substr(param_end + 1));
+							return;
+							break;
+						case ' ':
+							params.push_back(text.substr(position, param_end - position));
+							position = param_end + 1;
+							break;
+					}
+				}
+			}
+		}
+};
 
-    class nick: public Message
-    {
-        public:
-            nick(string name);
-    };
+class message {
+	public:
+		string prefix;
 
-    class user: public Message
-    {
-        public:
-            user(string user_name, string real_name);
-    };
+		message(string prefix="") : prefix(prefix) {}
+		virtual ~message() {}
+		virtual string serialize() const {
+			string serialized;
+			if (!prefix.empty())
+				serialized += string{':'} + prefix + " ";
+			return serialized;
+		}
+};
 
-    class pong: public Message
-    {
-        public:
-            pong(string server);
-    };
-}
+class pass : public message {
+	public:
+		static constexpr char command[] = "PASS";
+		string password;
+
+		pass(string prefix, string password)
+			: message(prefix), password(password) {}
+		pass(string password)
+			: pass("", password) {}
+		pass(build_digest digest)
+			: pass(digest.prefix, digest.params.at(0)) {
+			if (digest.params.size() != 1)
+				throw std::invalid_argument(string{"Malformed "} + command + " message");
+		}
+		string serialize() const override {
+			return message::serialize() + command + " " + password;
+		}
+};
+
+class nick : public message {
+	public:
+		static constexpr char command[] = "NICK";
+		string nickname;
+
+		nick(string prefix, string nickname)
+			: message(prefix), nickname(nickname) {}
+		nick(string nickname)
+			: nick("", nickname) {}
+		nick(build_digest digest)
+			: nick(digest.prefix, digest.params.at(0)) {
+			if (digest.params.size() != 1)
+				throw std::invalid_argument(string{"Malformed "} + command + " message");
+		}
+		string serialize() const override {
+			return message::serialize() + command + " " + nickname;
+		}
+};
+
+class user : public message {
+	public:
+		static constexpr char command[] = "USER";
+		unsigned mode;
+		string username;
+		string realname;
+
+		user(string prefix, string username, string realname, unsigned mode=0)
+			: message(prefix), username(username), realname(realname), mode(mode) {}
+		user(string username, string realname, unsigned mode=0)
+			:user("", username, realname, mode) {}
+		user(build_digest digest)
+			: user(digest.prefix, digest.params.at(0), digest.params.at(3)) {
+			if (digest.params.size() != 4)
+				throw std::invalid_argument(string{"Malformed "} + command + " message");
+		}
+		string serialize() const override {
+			string serialized = message::serialize() + command + " " + username + " " + std::to_string(mode) + " * ";
+			if (string::npos != realname.find_first_of(' '))
+				serialized += ":";
+			return serialized + realname;
+		}
+};
+
+class ping : public message {
+	public:
+		static constexpr char command[] = "PING";
+		vector<string> servers;
+
+		ping(string prefix, vector<string> servers)
+			: message(prefix), servers(servers) {
+			if (servers.empty())
+				throw std::invalid_argument(string{"Malformed "} + command + " message");
+		}
+		ping(vector<string> servers)
+			: ping("", servers) {}
+		ping(build_digest digest)
+			: ping(digest.prefix, digest.params) {}
+		string serialize() const override {
+			string serialized = message::serialize() + command;
+			for (auto server: servers)
+				serialized += " " + server;
+			return serialized;
+		}
+};
+
+class pong : public message {
+	public:
+		static constexpr char command[] = "PONG";
+		vector<string> servers;
+
+		pong(string prefix, vector<string> servers)
+			: message(prefix), servers(servers) {
+			if (servers.empty())
+				throw std::invalid_argument(string{"Malformed "} + command + " message");
+		}
+		pong(vector<string> servers)
+			: pong("", servers) {}
+		pong(build_digest digest)
+			: pong(digest.prefix, digest.params) {}
+		string serialize() const override {
+			string serialized = message::serialize() + command;
+			for (auto server: servers)
+				serialized += " " + server;
+			return serialized;
+		}
+};
+
+class notice: public message
+{
+	public:
+		static constexpr char command[] = "NOTICE";
+		string message_target;
+		string text;
+
+		notice(string prefix, string message_target, string text)
+			: message(prefix), message_target(message_target), text(text) {}
+		notice(string message_target, string text)
+			: notice("", message_target, text) {}
+		notice(build_digest digest)
+			: notice(digest.prefix, digest.params.at(0), digest.params.at(1)) {}
+		string serialize() const override {
+			return message::serialize() + command + " " + message_target + " " + text;
+		}
+};
+
+class builder {
+	public:
+		virtual ~builder() {}
+		virtual builder *set_next(builder *delegate) = 0;
+		virtual unique_ptr<message> build(string text) const = 0;
+};
+
+
+class abstract_builder : public builder {
+	private:
+		builder *next_builder = nullptr;
+	public:
+		~abstract_builder() {
+			delete next_builder;
+		}
+		builder *set_next(builder *delegate) override {
+			next_builder = delegate;
+			return delegate;
+		}
+		unique_ptr<message> build(string text) const override {
+			if (next_builder)
+				return next_builder->build(text);
+			throw std::invalid_argument(
+				"Unsupported message type: " + text);
+		}
+};
+
+template<typename T>
+class template_builder : public abstract_builder {
+	public:
+		unique_ptr<message> build(string text) const override {
+			build_digest digest(text);
+			if (digest.command != T::command)
+				return abstract_builder::build(text);
+
+			return make_unique<T>(digest);
+		}
+};
